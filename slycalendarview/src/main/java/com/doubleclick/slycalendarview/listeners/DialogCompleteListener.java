e9d032dd479b9cec6c2e4447912a6e55f520db00
CoderEslam <eslamghazy600@gmail.com>
2023-03-29 16:32:38 +0200

    Sly Calender View

---
+++ slycalendarview/src/main/java/com/doubleclick/slycalendarview/listeners/DialogCompleteListener.java
@@ -0,0 +1,11 @@
+package com.doubleclick.slycalendarview.listeners;
+
+/**
+ * Created by psinetron on 03/12/2018.
+ * http://slybeaver.ru
+ */
+public interface DialogCompleteListener {
+
+    void complete();
+
+}
